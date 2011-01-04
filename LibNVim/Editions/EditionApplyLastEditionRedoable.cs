1b37791cb2ce2ab2b007b82e6f12522d53b1bbfc
joycode <lethway@gmail.com>
2011-01-04 17:43:04 +0800

    bug fixs on 'J', 'o', 'O' add support for '.'

---
+++ LibNVim/Editions/EditionApplyLastEditionRedoable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibNVim.Editions
+{
+    class EditionApplyLastEditionRedoable : AbstractVimEdition
+    {
+        public EditionApplyLastEditionRedoable(Interfaces.IVimHost host, int repeat)
+            : base(host, repeat)
+        {
+        }
+
+        public override bool Apply()
+        {
+            if (this.Host.LastEdition != null) {
+                for (int i = 0; i < this.Repeat; i++) {
+                    if (!this.Host.LastEdition.Apply()) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
